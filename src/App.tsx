55898cb70f932ce8b6e71224e49e9b09dd9338e1
Nathan Walston <walston@users.noreply.github.com>
2024-09-26 21:52:39 -0700

    major styling and filehandling update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import { useState } from "react";
+import { Player } from "./Player";
 import "./App.css";
 
 function App() {
-  const [files, setFiles] = useState<File[]>([]);
+  const [files, setFiles] = useState<File[]>(new Array(6).fill(null));
+  const handleDragStart = (e: React.DragEvent, index: number) => {
+    e.dataTransfer.setData("text/plain", index.toString());
+  };
+  const handleDrop = (e: React.DragEvent, targetIndex: number) => {
+    const sourceIndex = Number(e.dataTransfer.getData("text/plain"));
+    const newFiles = [...files];
+    const [movedFile] = newFiles.splice(sourceIndex, 1);
+    newFiles.splice(targetIndex, 0, movedFile);
+    setFiles(newFiles);
+  };
   return (
-    <>
-      <input
-        type="file"
-        multiple
-        onChange={(e) => {
-          const files = Array.from(e.target.files!);
-          setFiles(files);
-        }}
-      />
-      <ul>
-        {files.map((file) => (
-          <li key={file.name}>{file.name}</li>
+    <form onSubmit={(e) => e.preventDefault()}>
+      <ul className="players">
+        {files.map((file, i) => (
+          <li
+            key={i}
+            draggable
+            onDragStart={(e) => handleDragStart(e, i)}
+            onDragOver={(e) => e.preventDefault()}
+            onDrop={(e) => handleDrop(e, i)}
+          >
+            <Player
+              file={file}
+              onChange={(file) => {
+                const newFiles = [...files];
+                newFiles[i] = file;
+                setFiles(newFiles);
+              }}
+            />
+          </li>
         ))}
       </ul>
-    </>
+    </form>
   );
 }
 
--- src/Player.tsx
+++ src/Player.tsx
@@ -1,20 +1,52 @@
-import React from "react";
+import { useRef } from "react";
+import "./Player.css";
 
 interface PlayerProps {
-  file: string | null;
-  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  file: File | null;
+  onChange: (file: File) => void;
 }
 
-const Player: React.FC<PlayerProps> = ({ file, onChange }) => {
-  if (!file) {
-    return <input type="file" accept="audio/mpeg" onChange={onChange} />;
-  }
-  return (
-    <audio controls loop>
-      <source src={file} type="audio/mpeg" />
-      Your browser does not support the audio element.
-    </audio>
+export function Player({ file, onChange }: PlayerProps) {
+  const uploadRef = useRef<HTMLInputElement>(null);
+  const button = (
+    <>
+      <button
+        className="upload-button"
+        onClick={() => uploadRef.current?.click()}
+      >
+        Import
+      </button>
+      <input
+        ref={uploadRef}
+        type="file"
+        accept="audio/*"
+        onChange={(e) => {
+          if (e.target.files && e.target.files[0]) {
+            onChange(e.target.files[0]);
+          }
+        }}
+      />
+    </>
   );
-};
+
+  if (!file) {
+    return button;
+  }
+
+  console.log("file", file);
+  return (
+    <>
+      <div>
+        <h5 className="filename">{file.name}</h5>
+        <audio controls loop>
+          <source src={URL.createObjectURL(file)} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+
+      {button}
+    </>
+  );
+}
 
 export default Player;
